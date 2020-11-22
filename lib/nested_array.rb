8e321fa8171a312d6c3167a54027ad542eedcb81
Matthew Kindt <matthewkindt@yahoo.com>
2020-11-22 06:04:13 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -10,8 +10,8 @@ ORGANIC_PRODUCE = [
 
 CONVENTIONAL_PRODUCE = [
   "Grapefruit",
-  "Oranges",
   "Pineapple",
+  "Oranges",
   "Watermelon",
   "Eggplant"
 ]
@@ -32,14 +32,17 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  assembled_matrix[1][1]
-  sorted_matrix[1][1]
+  matrix[1][1]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
 
 def matrix_update(matrix, row, column, new_value)
+  matrix[row][column] = new_value
+  matrix
+  matrix[row][column] = new_value
+  matrix
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-end
+end 
